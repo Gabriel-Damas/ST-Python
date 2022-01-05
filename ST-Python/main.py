5922e5a176626f582bb16a7dc3140532e9003c2e
Gabriel Damasceno <76968249+Gabriel-Damas@users.noreply.github.com>
2022-01-05 10:21:44 -0300

    Add files via upload

---
+++ ST-Python/main.py
@@ -0,0 +1,13 @@
+a = 5
+b = 10
+soma = a + b
+quadrado = soma**2
+print('A soma é', quadrado)
+
+a = 5
+b = 10
+soma = a + b
+if soma ==8:
+    print( 'A soma é 8')
+else:
+    print('A soma não é 8')
